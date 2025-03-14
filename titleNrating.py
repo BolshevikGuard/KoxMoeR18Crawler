8d5c7cb1dfa3849c49161ebef43fa6b176a087a0
Wei Zeng <81786865+BolshevikGuard@users.noreply.github.com>
2025-03-14 15:40:41 +0800

    Update titleNrating.py

--- titleNrating.py
+++ titleNrating.py
@@ -1,41 +1,145 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import random
+import sys
+import time
 
-# 目标网址（示例）
-url = "https://kox.moe/c/2144de.htm"
+# 目标网址加载
+urls = []
+with open('url_list_1.txt', 'r', encoding='utf-8') as f:
+    for idx, line in enumerate(f):
+        urls.append(line.strip())
+        print('\r', end='')
+        print(f'{idx+1} urls loaded', end='')
+        # time.sleep(0.001)
+
+print('\n', end='')
+
+user_agent = [
+    # Opera
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60",
+    "Opera/8.0 (Windows NT 5.1; U; en)",
+    "Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50",
+
+    # Firefox
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
+    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+
+    # Safari
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+
+    # chrome
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16",
+
+    # 360
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+
+
+    # 淘宝浏览器
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+
+    #猎豹浏览器
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)" ,
+
+    # QQ浏览器
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E) ",
+
+    # sogou浏览器
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)",
+
+    # maxthon浏览器
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36",
+
+    # UC浏览器
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36",
+
+
+
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36",
+
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X; zh-CN) AppleWebKit/537.51.1 (KHTML, like Gecko) Mobile/17D50 UCBrowser/12.8.2.1268 Mobile AliApp(TUnionSDK/0.1.20.3)",
+
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
+
+    "Mozilla/5.0 (Linux; Android 8.1.0; OPPO R11t Build/OPM1.171019.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/76.0.3809.89 Mobile Safari/537.36 T7/11.19 SP-engine/2.15.0 baiduboxapp/11.19.5.10 (Baidu; P1 8.1.0)",
+
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.14.0 main%2F1.0 baiduboxapp/11.18.0.16 (Baidu; P2 13.3.1) NABar/0.0 ",
+
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
+
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.10(0x17000a21) NetType/4G Language/zh_CN",
+
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
+
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36"
+]
 
 # 发送请求
-headers = {"User-Agent": "Mozilla/5.0"}
-response = requests.get(url, headers=headers)
+start_idx = 0
+r18_cnt = 0
+r15_cnt = 0
+with open('output.txt', 'a+', encoding='utf-8') as f:
+    for idx, url in enumerate(urls):
+        if idx < start_idx:
+            continue
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
+        headers = {"User-Agent": random.choice(user_agent)}
+        response = requests.get(url, headers=headers)
 
-    # 提取评分
-    score_tag = soup.find("table", class_="book_score")
-    score = None
-    if score_tag:
-        font_tag = score_tag.find("font", style=re.compile("font-size"))
-        if font_tag:
-            score = font_tag.text.strip()
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
 
-    # 提取漫画名称
-    title = None
-    meta_tag = soup.find("meta", {"name": "keywords"})
-    if meta_tag:
-        title = meta_tag["content"].split(",")[0]  # 取第一个关键词作为名称
+            # 提取评分
+            score_tag = soup.find("table", class_="book_score")
+            score = None
+            if score_tag:
+                font_tag = score_tag.find("font", style=re.compile("font-size"))
+                if font_tag:
+                    score = font_tag.text.strip()
 
-    # 提取 R15 / R18 分类
-    script_tag = soup.find("script", string=re.compile(r"var is_r18\s*=\s*parseInt"))
-    category = None
-    if script_tag:
-        match = re.search(r"var is_r18\s*=\s*parseInt\(\s*\"(\d+)\"\s*\)", script_tag.text)
-        if match:
-            category = int(match.group(1))
+            # 提取漫画名称
+            title = None
+            meta_tag = soup.find("meta", {"name": "keywords"})
+            if meta_tag:
+                title = meta_tag["content"].split(",")[0]  # 取第一个关键词作为名称
 
-    # 只保留 R15 (1) 和 R18 (2) 的漫画
-    if category in [1, 2]:
-        print(f"名称: {title}")
-        print(f"评分: {score}")
-        print(f"类别: {'R15' if category == 1 else 'R18'}")
+            # 提取 R15 / R18 分类
+            script_tag = soup.find("script", string=re.compile(r"var is_r18\s*=\s*parseInt"))
+            category = None
+            if script_tag:
+                match = re.search(r"var is_r18\s*=\s*parseInt\(\s*\"(\d+)\"\s*\)", script_tag.text)
+                if match:
+                    category = int(match.group(1))
+
+            # 只保留 R15 (1) 和 R18 (2) 的漫画
+            if category == 1:
+                r15_cnt += 1
+                f.write(f'{category} {title} {score} {url}\n')
+            elif category == 2:
+                r18_cnt += 1
+                f.write(f'{category} {title} {score} {url}\n')
+        
+        print('\r', end='')
+        print(f'id {idx} tested / {r18_cnt} r18 / {r15_cnt} r15', end='')
+        time.sleep(random.uniform(0.5, 1))
